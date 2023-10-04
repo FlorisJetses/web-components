c60fc480f7e463c187d6f0adf71be13e44e2e341
Floris Jetses <floris.jetses@deptagency.com>
2023-10-04 12:36:02 +0200

    chore: add comments, image and readme

---
+++ README.md
@@ -0,0 +1,3 @@
+Ideetjes voor wat je kan maken:
+-Een knop met een label, een icoon en een onclick die een alert afvuurt (misschien met custom text voor in de alert)
+-card component met een afbeelding, titel en beschrijving
--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,23 @@ class WebComponent extends HTMLElement {
     const shadow = this.attachShadow({mode: "open"})
     shadow.append(template.content.cloneNode(true))
   }
+  connectedCallback (){
+    // vuurt af wanneer het element aan de DOM is vastgemaakt
+  }
+  disconnectedCallback() {
+    // vuurt af wanneer het element uit de DOM wordt gehaald
+  }
+  attributeChangedCallback(name: string, oldValue: any, newValue: any) {
+    // wordt aangeroepen wanneer geobserveerde waardes veranderen
+    console.log(`${name} has changed from ${oldValue} to ${newValue}`)
+  }
 
+  static get observedAttributes() {
+    // array van strings met attributes die je wilt oberserveren
+    // <web-components title="een titel"></web-components>
+    // ['title']
+    return [];
+  }
 }
 
 customElements.define('web-component', WebComponent)
